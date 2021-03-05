a497fc990e50b40f3ff992d5b95d8633ef3e75f5
Rahim Alwer <rahim.alwer@gmail.com>
2021-03-05 13:33:28 +1100

    feat: update bundle to include new components

--- src/bundle/elements.ts
+++ src/bundle/elements.ts
@@ -1,3 +1,7 @@
 import '../providers/video/vds-video';
+import '../providers/hls/vds-hls';
+import '../ui/ui/vds-ui';
+import '../ui/controls/control/vds-control';
+import '../ui/controls/toggle/vds-toggle';
 
 export * from './index';
--- src/bundle/index.ts
+++ src/bundle/index.ts
@@ -1,3 +1,7 @@
 export * from '../core';
 export * from '../providers/file';
+export * from '../providers/hls';
 export * from '../providers/video';
+export * from '../ui/controls/control';
+export * from '../ui/controls/toggle';
+export * from '../ui/ui';
