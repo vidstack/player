17ab97d3da2e3df745cbb7b1659f0ca17d709f9e
Rahim Alwer <rahim.alwer@gmail.com>
2021-07-05 22:15:12 +1000

    docs(ui/controls): update `ControlsElement` class docs

--- src/ui/controls/controls/ControlsElement.js
+++ src/ui/controls/controls/ControlsElement.js
@@ -13,7 +13,41 @@ import { controlsElementStyles } from './styles.js';
 export const CONTROLS_ELEMENT_TAG_NAME = 'vds-controls';
 
 /**
+ * Container for holding individual media controls.
+ *
+ * ⚠️ **IMPORTANT:** The styling is left to you, it will only apply the following attributes:
+ *
+ * - `hidden`: Applied when the controls should be hidden and not available to the user.
+ * - `idle`: Applied when there is no user activity for a given period, `hidden` should have greater priority.
+ * - `media-can-play`: Applied when media can begin playback.
+ * - `media-paused`: Applied when media is paused.
+ * - `media-view-type`: Applied with the media view type such as `audio` or `video`.
+ *
  * @tagname vds-controls
+ * @slot Used to pass in controls.
+ * @example
+ * ```html
+ * <vds-controls>
+ *   <vds-play-button></vds-play-button>
+ *   <vds-scrubber></vds-scrubber>
+ *   <vds-fullscreen-button></vds-fullscreen-button>
+ * </vds-controls>
+ * ```
+ * @example
+ * ```css
+ * vds-controls {
+ *   opacity: 1;
+ *   transition: opacity 0.3s ease-out;
+ * }
+ *
+ * vds-controls[hidden] {
+ *   display: none;
+ * }
+ *
+ * vds-controls[idle] {
+ *   opacity: 0;
+ * }
+ * ```
  */
 export class ControlsElement extends VdsElement {
   /** @type {import('lit').CSSResultGroup} */
