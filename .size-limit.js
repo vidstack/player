ee5ac370b1c6c3ed994167fe442b046b3c08a444
Rahim Alwer <rahim.alwer@gmail.com>
2021-08-03 10:24:23 +1000

    chore(ci): add size limit checks

---
+++ .size-limit.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { sync: globSync } = require('fast-glob');
+
+const elements = globSync('src/**/*Element.ts')
+  .filter(
+    (filePath) => !filePath.includes('test') && !filePath.includes('src/base')
+  )
+  .map((filePath) => path.basename(filePath, path.extname(filePath)))
+  .map((element) => ({
+    name: `import { ${element} } from '@vidstack/elements'`,
+    limit: '12 kB',
+    path: 'dist-prod/bundle/index.js',
+    import: `{ ${element} }`
+  }));
+
+module.exports = [
+  {
+    name: "import '@vidstack/elements'",
+    limit: '35 kB',
+    path: 'dist-prod/bundle/index.js'
+  },
+  {
+    name: "import '@vidstack/elements/define'",
+    limit: '35 kB',
+    path: 'dist-prod/bundle/define.js'
+  },
+  ...elements
+];
