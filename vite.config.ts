766050cf58ab274026cc3f7de5fbfef588569eef
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-05 13:11:08 +1100

    chore: setup vitest

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config
+export default defineConfig({
+  define: {
+    __DEV__: 'false'
+  },
+  // https://vitest.dev/config
+  test: {
+    include: ['src/**/*.test.ts'],
+    environment: 'jsdom'
+  }
+});
