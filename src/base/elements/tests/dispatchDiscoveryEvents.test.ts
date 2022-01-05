9e387cce1e58c3e3037a1d6bd2154ede1fb1530a
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-05 14:33:59 +1100

    add `base/elements` tests

---
+++ src/base/elements/tests/dispatchDiscoveryEvents.test.ts
@@ -0,0 +1,35 @@
+import { oneEvent } from '@open-wc/testing-helpers';
+import { LitElement } from 'lit';
+
+import { isFunction } from '../../../utils/unit';
+import { dispatchDiscoveryEvents } from '../discovery';
+
+class FakeElement extends LitElement {
+  constructor() {
+    super();
+    // Test event type `vds-media-controller-connect` randomly chosen for testing.
+    dispatchDiscoveryEvents(this, 'vds-media-controller-connect');
+  }
+}
+
+window.customElements.define('fake-el', FakeElement);
+
+test('it should dispatch discovery event', async () => {
+  const el = document.createElement('fake-el') as FakeElement;
+
+  setTimeout(() => {
+    document.body.append(el);
+  }, 0);
+
+  const { detail } = await oneEvent(window, 'vds-media-controller-connect');
+
+  expect(detail.element).to.equal(el);
+  expect(isFunction(detail.onDisconnect)).to.be.true;
+
+  const disconnectSpy = vi.fn();
+  detail.onDisconnect(disconnectSpy);
+
+  el.remove();
+
+  expect(disconnectSpy).to.toHaveBeenCalledOnce();
+});
