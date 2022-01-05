3d550eb1ae9619fdf446b0468165eadfbd2075f5
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-05 21:10:35 +1100

    add `base/queue` tests

--- src/base/events/tests/hostedEventListener.test.ts
+++ src/base/events/tests/hostedEventListener.test.ts
@@ -1,3 +1,35 @@
-test('', () => {
-  //
+import { fixture, nextFrame } from '@open-wc/testing-helpers';
+import { html, LitElement } from 'lit';
+
+import { hostedEventListener } from '../hostedEventListener';
+
+class FakeElement extends LitElement {
+  listener = vi.fn();
+
+  handleClick = hostedEventListener(this, 'click', this.listener);
+
+  override render() {
+    return html`<slot></slot>`;
+  }
+}
+
+window.customElements.define('fake-el', FakeElement);
+
+test('it should attach event listener', async () => {
+  const el = await fixture<FakeElement>(html`<fake-el></fake-el>`);
+  const child = document.createElement('div');
+
+  el.appendChild(child);
+
+  await nextFrame();
+
+  child.dispatchEvent(
+    new MouseEvent('click', { composed: true, bubbles: true })
+  );
+
+  child.dispatchEvent(
+    new MouseEvent('click', { composed: true, bubbles: true })
+  );
+
+  expect(el.listener).to.toHaveBeenCalledTimes(2);
 });
