bd2efddd10007e536ca68696cab3e8f9c765af00
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-13 09:36:17 +1100

    chore: wip

---
+++ .vitebook/.cache/svelte_store.js
@@ -0,0 +1,4 @@
+import { derived, readable, writable } from './chunk-LJOTWLM6.js';
+import { get_store_value } from './chunk-KO5NNRVU.js';
+export { derived, get_store_value as get, readable, writable };
+//# sourceMappingURL=svelte_store.js.map
---
+++ .vitebook/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { SvelteComponent } from 'svelte';
+
+  export let component: typeof SvelteComponent;
+</script>
+
+<svelte:component this={component} />
+
+<style>
+  :global(.navbar__logo) {
+    display: flex;
+    align-items: center;
+  }
+</style>
