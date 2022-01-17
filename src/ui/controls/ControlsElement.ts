baa2dc4b59ddd21e6bc0c6b9997b794cbc461681
Rahim Alwer <rahim.alwer@gmail.com>
2022-01-17 22:45:21 +1100

    feat: add media styling attrs to `ControlsElement`

--- src/ui/controls/ControlsElement.ts
+++ src/ui/controls/ControlsElement.ts
@@ -1,6 +1,8 @@
 import { CSSResultGroup, html, LitElement, TemplateResult } from 'lit';
 
 import { logElementLifecycle } from '../../base/logger';
+import { hostedMediaStoreSubscription } from '../../media';
+import { setAttribute } from '../../utils/dom';
 import { controlsElementStyles } from './styles';
 
 /**
@@ -8,13 +10,12 @@ import { controlsElementStyles } from './styles';
  *
  * 💡 The following attributes are updated for your styling needs:
  *
- * - `hidden`: Applied when the controls should be hidden and not available to the user.
- * - `idle`: Applied when there is no user activity for a set period, `hidden` should have greater priority.
- * - `media-autoplay-error`: Applied when media autoplay fails. It can be used to show recovery UI
- * if controls are hidden.
+ * - `media-idle`: Applied when there is no user activity for a set period of time.
  * - `media-can-play`: Applied when media can begin playback.
  * - `media-paused`: Applied when media is paused.
  * - `media-view-type`: Applied with the media view type such as `audio` or `video`.
+ * - `media-autoplay-error`: Applied when media autoplay fails. It can be used to show recovery UI
+ * if controls are hidden.
  *
  * @tagname vds-controls
  * @slot Used to pass in controls.
@@ -33,7 +34,8 @@ import { controlsElementStyles } from './styles';
  *   transition: opacity 0.3s ease-out;
  * }
  *
- * vds-controls[idle] {
+ * vds-controls[idle],
+ * vds-controls:not([media-can-play]) {
  *   opacity: 0;
  * }
  * ```
@@ -45,9 +47,26 @@ export class ControlsElement extends LitElement {
 
   constructor() {
     super();
+
     if (__DEV__) {
       logElementLifecycle(this);
     }
+
+    hostedMediaStoreSubscription(this, 'canPlay', ($canPlay) => {
+      setAttribute(this, 'media-can-play', $canPlay);
+    });
+    hostedMediaStoreSubscription(this, 'idle', ($idle) => {
+      setAttribute(this, 'media-idle', $idle);
+    });
+    hostedMediaStoreSubscription(this, 'paused', ($paused) => {
+      setAttribute(this, 'media-paused', $paused);
+    });
+    hostedMediaStoreSubscription(this, 'viewType', ($viewType) => {
+      setAttribute(this, 'media-view-type', $viewType);
+    });
+    hostedMediaStoreSubscription(this, 'autoplayError', ($autoplayError) => {
+      setAttribute(this, 'media-autoplay-error', !!$autoplayError);
+    });
   }
 
   protected override render(): TemplateResult {
