45bd1d806e59b10263f5b6b14b31b709afdf9477
Rahim Alwer <rahim.alwer@gmail.com>
2022-03-07 23:12:40 +1100

    chore: downgrade `esbuild` to `0.14.23` - build issues

--- src/react/components/AspectRatio.ts
+++ src/react/components/AspectRatio.ts
@@ -8,16 +8,15 @@ import { AspectRatioElement } from '../../ui/aspect-ratio';
 const EVENTS = {} as const;
 
 /** This element creates a container that will hold the dimensions of the desired aspect ratio. This
-container is useful for reserving space for media (img, video, etc.) as it loads over the
-network.
+container is useful for reserving space for media as it loads over the network.
 
 💡  If your browser matrix supports the
 [`aspect-ratio`](https://developer.mozilla.org/en-US/docs/Web/CSS/aspect-ratio) CSS property
 then you can skip using this component, and set the desired aspect ratio directly on the
-element.
+provider.
 
-💡 By default it respects the browser's default width and height for media. This is not specific
-to the loaded media but instead a general setting of `1:2`. */
+💡 By default it respects the browser's default aspect-ratio for media. This is not specific
+to the loaded media but instead a general setting of `2/1`. */
 const AspectRatio = createComponent(
   React,
   'vds-aspect-ratio',
