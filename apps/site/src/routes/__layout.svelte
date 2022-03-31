978dd74d11a2e1bc8603ef043ee1174566cc529c
Rahim Alwer <rahim.alwer@gmail.com>
2022-03-31 15:18:16 +1100

    chore(site): smooth out page loading bar

--- apps/site/src/routes/__layout.svelte
+++ apps/site/src/routes/__layout.svelte
@@ -9,6 +9,7 @@
 
   import { onMount } from 'svelte';
   import { navigating } from '$app/stores';
+  import { browser } from '$app/env';
 
   onMount(() => {
     // Strange fix for strange issue -_O_- (`cmd + k` opening two docsearch containers).
@@ -24,7 +25,21 @@
 
   // https://github.com/rstacruz/nprogress#configuration
   NProgress.configure({ minimum: 0.16 });
-  $: NProgress[$navigating ? 'start' : 'done']();
+
+  let navigatingTimeout;
+
+  $: if (browser && $navigating) {
+    window.clearTimeout(navigatingTimeout);
+    navigatingTimeout = window.setTimeout(() => {
+      if (!$navigating) return;
+      NProgress.start();
+    }, 500);
+  }
+
+  $: if (browser && !$navigating) {
+    window.clearTimeout(navigatingTimeout);
+    NProgress.done();
+  }
 </script>
 
 <slot />
