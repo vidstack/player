e863567e9d886bf1bafdd01137fcf52bbe2053e3
Rahim Alwer <rahim.alwer@gmail.com>
2022-06-14 21:40:21 +1000

    fix(site): only scroll contain on larger screens

--- apps/site/src/layouts/DocsLayout.svelte
+++ apps/site/src/layouts/DocsLayout.svelte
@@ -60,11 +60,11 @@
     </svelte:fragment>
 
     <svelte:fragment slot="navbar-bottom">
-      <div class="border-gray-outline border-t flex mt-3 w-full pt-4 items-center 992:hidden">
+      <div class="border-gray-outline 992:hidden mt-3 flex w-full items-center border-t pt-4">
         <button
           id="main-sidebar-button"
           type="button"
-          class="rounded-md font-medium text-gray-soft text-sm p-2 inline-flex justify-center hover:text-gray-inverse"
+          class="text-gray-soft hover:text-gray-inverse inline-flex justify-center rounded-md p-2 text-sm font-medium"
           aria-controls="main-sidebar"
           aria-expanded={ariaBool(isSidebarOpen)}
           aria-haspopup="true"
@@ -87,13 +87,13 @@
           <MenuUnfoldIcon width="28" height="28" />
         </button>
         <ol
-          class="flex mt-px text-md text-gray-soft ml-2.5 leading-6 items-center whitespace-nowrap"
+          class="text-md text-gray-soft mt-px ml-2.5 flex items-center whitespace-nowrap leading-6"
         >
           <li class="flex items-center">
             {$activeCategory}
             <RightArrowIcon class="mx-1" width="16" height="16" />
           </li>
-          <li class="font-semibold text-slate-900 truncate dark:text-slate-200">
+          <li class="truncate font-semibold text-slate-900 dark:text-slate-200">
             {$activeLink?.title || $markdown?.title}
           </li>
         </ol>
@@ -105,7 +105,7 @@
         {search}
         class={({ open }) =>
           clsx(
-            'self-start fixed top-0 left-0 transform bg-gray-body z-50 border-gray-outline border-r scrollbar scroll-contain',
+            'bg-gray-body border-gray-outline scrollbar scroll-contain-mobile fixed top-0 left-0 z-50 transform self-start border-r',
             '-translate-x-full transform transition-transform duration-200 ease-out will-change-transform',
             'max-h-screen min-h-screen min-w-[var(--sidebar-min-width)] max-w-[var(--sidebar-max-width)]',
             '992:translate-x-0 922:block 992:sticky 992:z-0 overflow-y-auto p-[var(--sidebar-padding)]',
@@ -121,8 +121,8 @@
       </Sidebar>
     </svelte:fragment>
 
-    <div class="max-w-[var(--article-max-width)] z-10 markdown prose dark:prose-invert">
-      <p class="font-semibold text-brand mb-3.5 text-[15px] leading-6">
+    <div class="markdown prose dark:prose-invert z-10 max-w-[var(--article-max-width)]">
+      <p class="text-brand mb-3.5 text-[15px] font-semibold leading-6">
         {$activeCategory}
       </p>
 
@@ -132,9 +132,9 @@
     {#if $previousLink || $nextLink}
       <hr class="border-gray-divider mt-20" />
 
-      <div class="flex font-semibold text-lg pt-12 pb-20 text-gray-300 items-center 992:text-xl">
+      <div class="992:text-xl flex items-center pt-12 pb-20 text-lg font-semibold text-gray-300">
         {#if $previousLink}
-          <div class="flex flex-col mb-4 items-start">
+          <div class="mb-4 flex flex-col items-start">
             <span class="text-gray-inverse mb-4 ml-3 inline-block">Previous</span>
             <Button arrow="left" href={$previousLink.slug} class="hover:text-gray-inverse">
               {$previousLink.title}
@@ -143,7 +143,7 @@
         {/if}
 
         {#if $nextLink}
-          <div class="flex flex-col ml-auto mb-4 items-end">
+          <div class="ml-auto mb-4 flex flex-col items-end">
             <span class="text-gray-inverse mr-3 mb-4 inline-block">Next</span>
             <Button arrow="right" href={$nextLink.slug} class="hover:text-gray-inverse">
               {$nextLink.title}
@@ -158,7 +158,7 @@
 
       <OnThisPage
         class={clsx(
-          'pt-8 pb-8 hidden overflow-auto min-w-[160px] sticky right-4 pr-4 1440:right-6 1440:pr-2 1280:block pl-0.5',
+          '1440:right-6 1440:pr-2 1280:block sticky right-4 hidden min-w-[160px] overflow-auto pt-8 pb-8 pr-4 pl-0.5',
           'top-[var(--navbar-height)] max-h-[calc(100vh-var(--navbar-height))]',
         )}
       />
