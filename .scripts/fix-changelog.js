4e23e5dc2ddecf018bdc1a30a9ca0f6d81893765
Rahim Alwer <rahim.alwer@gmail.com>
2023-01-25 11:48:27 +1100

    chore: fix changelog

--- .scripts/fix-changelog.js
+++
@@ -1,7 +0,0 @@
-import { writeFileSync } from 'fs';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-
-const filePath = resolve(process.cwd(), 'CHANGELOG.md');
-const content = readFileSync(filePath).toString();
-writeFileSync(filePath, content.replace(/\.com\/(.*?)\/vidstack/g, '.com/vidstack/vidstack'));
