072586bcdb00448f3670baca24a1e4c53dac1989
Rahim Alwer <rahim.alwer@gmail.com>
2023-01-26 22:19:45 +1100

    feat(vidstack): include component doc links in vscode custom data

--- packages/vidstack/analyze.config.ts
+++ packages/vidstack/analyze.config.ts
@@ -3,15 +3,30 @@ import { createJSONPlugin, createVSCodePlugin } from '@maverick-js/compiler/anal
 export default [
   createJSONPlugin(),
   createVSCodePlugin({
+    transformTagData(component, data) {
+      const refs = (data.references ??= []);
+
+      component.doctags
+        ?.filter((tag) => (tag.name === 'docs' || tag.name === 'see') && tag.text)
+        .forEach((tag) => {
+          const w3c = tag.text!.includes('w3c.github') ? 'W3C' : undefined;
+          const mdn = tag.text!.includes('developer.mozilla') ? 'MDN' : undefined;
+          refs.push({ name: w3c ?? mdn ?? 'Documentation', url: tag.text! });
+        });
+
+      if (refs.length > 0) data.references = refs;
+
+      return data;
+    },
     transformAttributeData(prop, data) {
       const refs = (data.references ??= []);
 
       prop.doctags
-        ?.filter((tag) => tag.name === 'link' && tag.text)
+        ?.filter((tag) => (tag.name === 'see' || tag.name === 'link') && tag.text)
         .forEach((tag) => {
           const w3c = tag.text!.includes('w3c.github') ? 'W3C' : undefined;
           const mdn = tag.text!.includes('developer.mozilla') ? 'MDN' : undefined;
-          refs.push({ name: w3c ?? mdn ?? 'Link', url: tag.text! });
+          refs.push({ name: w3c ?? mdn ?? 'Documentation', url: tag.text! });
         });
 
       if (refs.length > 0) data.references = refs;
