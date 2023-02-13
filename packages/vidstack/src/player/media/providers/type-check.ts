775f074a5597b97cab87af0c1db4bfe425a47ac6
Rahim Alwer <rahim.alwer@gmail.com>
2023-02-13 16:16:22 +1100

    feat(vidstack): element type checks
    closes #741

--- packages/vidstack/src/player/media/providers/type-check.ts
+++ packages/vidstack/src/player/media/providers/type-check.ts
@@ -2,14 +2,27 @@ import { AUDIO_PROVIDER, type AudioProvider } from './audio/provider';
 import { HLS_PROVIDER, type HLSProvider } from './hls/provider';
 import { VIDEO_PROVIDER, type VideoProvider } from './video/provider';
 
+/** @see {@link https://www.vidstack.io/docs/player/providers/audio} */
 export function isAudioProvider(provider: unknown): provider is AudioProvider {
   return !!provider?.[AUDIO_PROVIDER];
 }
 
+/** @see {@link https://www.vidstack.io/docs/player/providers/video} */
 export function isVideoProvider(provider: unknown): provider is VideoProvider {
   return !!provider?.[VIDEO_PROVIDER];
 }
 
+/** @see {@link https://www.vidstack.io/docs/player/providers/hls} */
 export function isHLSProvider(provider: unknown): provider is HLSProvider {
   return !!provider?.[HLS_PROVIDER];
+}
+
+/** @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement} */
+export function isHTMLAudioElement(element: unknown): element is HTMLAudioElement {
+  return !__SERVER__ && element instanceof HTMLAudioElement;
+}
+
+/** @see {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement} */
+export function isHTMLVideoElement(element: unknown): element is HTMLVideoElement {
+  return !__SERVER__ && element instanceof HTMLVideoElement;
 }
