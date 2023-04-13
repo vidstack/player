995d82d62d8551758eb991826250913d78be5d63
Rahim Alwer <rahim.alwer@gmail.com>
2023-04-13 12:41:05 +1000

    fix: use width to scale slider fill/progress tracks

--- apps/site/app/docs/[lib]/player/[4]styling/[3]tailwind/.snippets/react/tw-good.tsx
+++ apps/site/app/docs/[lib]/player/[4]styling/[3]tailwind/.snippets/react/tw-good.tsx
@@ -14,7 +14,7 @@ function Player() {
 
       {/* filled playback track where color changes if buffering (i.e., waiting) */}
       <div className="relative h-6 w-full bg-gray-200">
-        <div className="waiting:bg-sky-500 absolute top-0 left-0 h-full w-full origin-left scale-x-[calc(var(--media-current-time)/var(--media-duration))] transform bg-gray-400 will-change-transform"></div>
+        <div className="waiting:bg-sky-500 absolute top-0 left-0 h-full w-full origin-left scale-x-[calc(var(--media-current-time)/var(--media-duration))] bg-gray-400 will-change-transform"></div>
       </div>
     </MediaPlayer>
   );
--- apps/site/app/docs/[lib]/player/components/sliders/[5]slider-thumbnail/.snippets/tailwind.tsx
+++ apps/site/app/docs/[lib]/player/components/sliders/[5]slider-thumbnail/.snippets/tailwind.tsx
@@ -13,7 +13,7 @@ function SliderPreview({ children }) {
   // slot to help with positioning. See the "Slider > CSS Variables" section for more information.
   return (
     <div
-      className="absolute top-[var(--preview-top)] left-[var(--preview-left)] flex -translate-x-1/2 transform items-center justify-center rounded-sm bg-black px-2.5 py-1 text-white/80 opacity-0 transition-opacity duration-200 ease-out group-data-[interactive]:opacity-100 group-data-[interactive]:ease-in"
+      className="absolute top-[var(--preview-top)] left-[var(--preview-left)] flex -translate-x-1/2 items-center justify-center rounded-sm bg-black px-2.5 py-1 text-white/80 opacity-0 transition-opacity duration-200 ease-out group-data-[interactive]:opacity-100 group-data-[interactive]:ease-in"
       slot="preview"
     >
       {children}
