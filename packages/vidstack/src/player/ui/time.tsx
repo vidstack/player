f7bbb81274f3a306db4aaf7d1dfcc603eef4c25f
Hydrogen <kenhydrogen@gmail.com>
2023-05-16 20:22:32 +0200

    fix: remove console log

--- packages/vidstack/src/player/ui/time.tsx
+++ packages/vidstack/src/player/ui/time.tsx
@@ -59,8 +59,6 @@ export class Time extends Component<TimeAPI> {
       const frameRate = this._media.$props.frameRate;
       const frame = Math.floor(Number.parseFloat(seconds.toFixed(5)) * frameRate());
 
-      console.log(frame);
-
       return formatTimeSmpte(frame, time, frameRate(), padHours(), padMinutes(), showHours());
     }
 
