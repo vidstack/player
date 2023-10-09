afcac0d8359d19d971cf00101ff758879cf66280
Rahim Alwer <rahim.alwer@gmail.com>
2023-10-09 11:17:47 +1100

    fix(player): remounting default layout breaks

--- packages/vidstack/src/elements/lit/lit-element.ts
+++ packages/vidstack/src/elements/lit/lit-element.ts
@@ -4,18 +4,16 @@ export class LitElement extends HTMLElement {
   rootPart: RootPart | null = null;
 
   connectedCallback() {
-    if (!this.rootPart && (this as unknown as LitRenderer).render) {
-      this.rootPart = render((this as unknown as LitRenderer).render(), this, {
-        renderBefore: this.firstChild,
-      });
-    }
-
-    this.rootPart?.setConnected(true);
+    this.rootPart = render((this as unknown as LitRenderer).render(), this, {
+      renderBefore: this.firstChild,
+    });
+    this.rootPart.setConnected(true);
   }
 
   disconnectedCallback() {
     this.rootPart?.setConnected(false);
     this.rootPart?._$setValue(null);
+    this.rootPart = null;
   }
 }
 
