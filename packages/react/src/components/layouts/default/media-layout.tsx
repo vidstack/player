e6c9b83f1384db930b38e7357a69f794da028ce9
Rahim Alwer <rahim.alwer@gmail.com>
2024-01-30 18:23:49 +1100

    fix(player): layout props missing in analysis

--- packages/react/src/components/layouts/default/media-layout.tsx
+++ packages/react/src/components/layouts/default/media-layout.tsx
@@ -3,9 +3,10 @@ import * as React from 'react';
 import { useSignal } from 'maverick.js/react';
 import { isBoolean } from 'maverick.js/std';
 import {
-  type DefaultLayoutProps as BaseLayoutProps,
+  type DefaultLayoutTranslations,
   type MediaPlayerQuery,
   type MediaStreamType,
+  type ThumbnailSrc,
 } from 'vidstack';
 
 import { useMediaContext } from '../../../hooks/use-media-context';
@@ -19,9 +20,7 @@ import type { DefaultLayoutIcons } from './icons';
  * DefaultMediaLayout
  * -----------------------------------------------------------------------------------------------*/
 
-export interface DefaultLayoutProps<Slots = unknown>
-  extends PrimitivePropsWithRef<'div'>,
-    Omit<Partial<BaseLayoutProps>, 'when' | 'smallWhen' | 'customIcons'> {
+export interface DefaultLayoutProps<Slots = unknown> extends PrimitivePropsWithRef<'div'> {
   children?: React.ReactNode;
   /**
    * The icons to be rendered and displayed inside the layout.
@@ -52,6 +51,48 @@ export interface DefaultLayoutProps<Slots = unknown>
    * @defaultValue `({ width, height }) => width < 576 || height < 380`
    */
   smallLayoutWhen?: boolean | MediaPlayerQuery;
+  /**
+   * The thumbnails resource.
+   *
+   * @see {@link https://www.vidstack.io/docs/wc/player/core-concepts/loading#thumbnails}
+   */
+  thumbnails?: ThumbnailSrc;
+  /**
+   * Translation map from english to your desired language for words used throughout the layout.
+   */
+  translations?: Partial<DefaultLayoutTranslations> | null;
+  /**
+   * Specifies whether menu buttons should be placed in the top or bottom controls group. This
+   * only applies to the large video layout.
+   */
+  menuGroup?: 'top' | 'bottom';
+  /**
+   * Whether modal menus should be disabled when the small layout is active. A modal menu is
+   * a floating panel that floats up from the bottom of the screen (outside of the player). It's
+   * enabled by default as it provides a better user experience for touch devices.
+   */
+  noModal?: boolean;
+  /**
+   * The minimum width of the slider to start displaying slider chapters when available.
+   */
+  sliderChaptersMinWidth?: number;
+  /**
+   * Whether the time slider should be disabled.
+   */
+  disableTimeSlider?: boolean;
+  /**
+   * Whether all gestures such as press to play or seek should not be active.
+   */
+  noGestures?: boolean;
+  /**
+   * Whether keyboard actions should not be displayed.
+   */
+  noKeyboardActionDisplay?: boolean;
+  /**
+   * The number of seconds to seek forward or backward when pressing the seek button or using
+   * keyboard shortcuts.
+   */
+  seekStep?: number;
   /**
    * Provide additional content to be inserted in specific positions.
    */
