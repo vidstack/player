3d3a27afe7970c54f60c4c1939743989938c3839
Rahim Alwer <rahim.alwer@gmail.com>
2024-04-19 17:52:28 +1000

    fix(player): prefer raf for scheduling jobs

--- packages/vidstack/src/utils/dom.ts
+++ packages/vidstack/src/utils/dom.ts
@@ -48,17 +48,27 @@ export function isEventInside(el: HTMLElement, event: Event) {
   return isDOMNode(event.target) && el.contains(event.target);
 }
 
-const intervalJobs = new Set<() => void>();
+const rafJobs = new Set<() => void>();
 
 if (!__SERVER__) {
-  window.setInterval(() => {
-    for (const job of intervalJobs) job();
-  }, 500);
+  function processJobs() {
+    for (const job of rafJobs) {
+      try {
+        job();
+      } catch (e) {
+        if (__DEV__) console.error(`[vidstack] failed job:\n\n${e}`);
+      }
+    }
+
+    window.requestAnimationFrame(processJobs);
+  }
+
+  processJobs();
 }
 
-export function scheduleIntervalJob(job: () => void) {
-  intervalJobs.add(job);
-  return () => intervalJobs.delete(job);
+export function scheduleRafJob(job: () => void) {
+  rafJobs.add(job);
+  return () => rafJobs.delete(job);
 }
 
 export function setAttributeIfEmpty(target: Element, name: string, value: string) {
@@ -106,7 +116,7 @@ export function checkVisibility(el: HTMLElement | null) {
 }
 
 export function observeVisibility(el: HTMLElement, callback: (isVisible: boolean) => void) {
-  return scheduleIntervalJob(() => callback(checkVisibility(el)));
+  return scheduleRafJob(() => callback(checkVisibility(el)));
 }
 
 export function isElementParent(
